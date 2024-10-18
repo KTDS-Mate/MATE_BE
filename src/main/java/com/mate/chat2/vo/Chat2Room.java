0a5af146cafaf5d39a6a7bba922e2448059f1684
YuriShin97 <phcomic@gmail.com>
2024-10-18 14:47:37 +0900

    feat: Chatting-Sample-Code-Test

---
+++ src/main/java/com/mate/chat2/vo/Chat2Room.java
@@ -0,0 +1,68 @@
+package com.mate.chat2.vo;
+
+public class Chat2Room {
+    private String chatRoomId;
+    private String title;
+    private String status;
+    private String usrId;
+
+    private String userName;
+    private int cnt;
+
+    public Chat2Room(String chatRoomId, String title, String status, String usrId, String userName, int cnt) {
+        this.chatRoomId = chatRoomId;
+        this.title = title;
+        this.status = status;
+        this.usrId = usrId;
+        this.userName = userName;
+        this.cnt = cnt;
+    }
+
+    public String getChatRoomId() {
+        return chatRoomId;
+    }
+
+    public void setChatRoomId(String chatRoomId) {
+        this.chatRoomId = chatRoomId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUsrId() {
+        return usrId;
+    }
+
+    public void setUsrId(String usrId) {
+        this.usrId = usrId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+}
---
+++ src/main/java/com/mate/user/vo/UserVO.java
@@ -0,0 +1,228 @@
+package com.mate.user.vo;
+
+public class UserVO {
+
+    private String usrId;
+    private String usrLgnId;
+    private String usrPwd;
+    private String usrLnm;
+    private String usrFnm;
+    private String usrGndr;
+    private String usrEml;
+    private String usrPhn;
+    private String usrBd;
+    private String usrAddrs;
+    private String usrIsGd;
+    private String usrSlfIntdctn;
+    private String usrPypEml;
+    private double usrAvgRtng;
+    private String usrJnDt;
+    private String usrClDt;
+    private String usrIsCl;
+    private String usrMdfyDt;
+    private String gdPrflImg;
+    private String gdIdImg;
+    private String gdCbcImg;
+    private String gdRpCntId;
+
+    public UserVO(String usrId, String usrLgnId, String usrPwd, String usrLnm, String usrFnm, String usrGndr, String usrEml, String usrPhn, String usrBd, String usrAddrs, String usrIsGd, String usrSlfIntdctn, String usrPypEml, double usrAvgRtng, String usrJnDt, String usrClDt, String usrIsCl, String usrMdfyDt, String gdPrflImg, String gdIdImg, String gdCbcImg, String gdRpCntId) {
+        this.usrId = usrId;
+        this.usrLgnId = usrLgnId;
+        this.usrPwd = usrPwd;
+        this.usrLnm = usrLnm;
+        this.usrFnm = usrFnm;
+        this.usrGndr = usrGndr;
+        this.usrEml = usrEml;
+        this.usrPhn = usrPhn;
+        this.usrBd = usrBd;
+        this.usrAddrs = usrAddrs;
+        this.usrIsGd = usrIsGd;
+        this.usrSlfIntdctn = usrSlfIntdctn;
+        this.usrPypEml = usrPypEml;
+        this.usrAvgRtng = usrAvgRtng;
+        this.usrJnDt = usrJnDt;
+        this.usrClDt = usrClDt;
+        this.usrIsCl = usrIsCl;
+        this.usrMdfyDt = usrMdfyDt;
+        this.gdPrflImg = gdPrflImg;
+        this.gdIdImg = gdIdImg;
+        this.gdCbcImg = gdCbcImg;
+        this.gdRpCntId = gdRpCntId;
+    }
+
+    public String getUsrId() {
+        return usrId;
+    }
+
+    public void setUsrId(String usrId) {
+        this.usrId = usrId;
+    }
+
+    public String getUsrLgnId() {
+        return usrLgnId;
+    }
+
+    public void setUsrLgnId(String usrLgnId) {
+        this.usrLgnId = usrLgnId;
+    }
+
+    public String getUsrPwd() {
+        return usrPwd;
+    }
+
+    public void setUsrPwd(String usrPwd) {
+        this.usrPwd = usrPwd;
+    }
+
+    public String getUsrLnm() {
+        return usrLnm;
+    }
+
+    public void setUsrLnm(String usrLnm) {
+        this.usrLnm = usrLnm;
+    }
+
+    public String getUsrFnm() {
+        return usrFnm;
+    }
+
+    public void setUsrFnm(String usrFnm) {
+        this.usrFnm = usrFnm;
+    }
+
+    public String getUsrGndr() {
+        return usrGndr;
+    }
+
+    public void setUsrGndr(String usrGndr) {
+        this.usrGndr = usrGndr;
+    }
+
+    public String getUsrEml() {
+        return usrEml;
+    }
+
+    public void setUsrEml(String usrEml) {
+        this.usrEml = usrEml;
+    }
+
+    public String getUsrPhn() {
+        return usrPhn;
+    }
+
+    public void setUsrPhn(String usrPhn) {
+        this.usrPhn = usrPhn;
+    }
+
+    public String getUsrBd() {
+        return usrBd;
+    }
+
+    public void setUsrBd(String usrBd) {
+        this.usrBd = usrBd;
+    }
+
+    public String getUsrAddrs() {
+        return usrAddrs;
+    }
+
+    public void setUsrAddrs(String usrAddrs) {
+        this.usrAddrs = usrAddrs;
+    }
+
+    public String getUsrIsGd() {
+        return usrIsGd;
+    }
+
+    public void setUsrIsGd(String usrIsGd) {
+        this.usrIsGd = usrIsGd;
+    }
+
+    public String getUsrSlfIntdctn() {
+        return usrSlfIntdctn;
+    }
+
+    public void setUsrSlfIntdctn(String usrSlfIntdctn) {
+        this.usrSlfIntdctn = usrSlfIntdctn;
+    }
+
+    public String getUsrPypEml() {
+        return usrPypEml;
+    }
+
+    public void setUsrPypEml(String usrPypEml) {
+        this.usrPypEml = usrPypEml;
+    }
+
+    public double getUsrAvgRtng() {
+        return usrAvgRtng;
+    }
+
+    public void setUsrAvgRtng(double usrAvgRtng) {
+        this.usrAvgRtng = usrAvgRtng;
+    }
+
+    public String getUsrJnDt() {
+        return usrJnDt;
+    }
+
+    public void setUsrJnDt(String usrJnDt) {
+        this.usrJnDt = usrJnDt;
+    }
+
+    public String getUsrClDt() {
+        return usrClDt;
+    }
+
+    public void setUsrClDt(String usrClDt) {
+        this.usrClDt = usrClDt;
+    }
+
+    public String getUsrIsCl() {
+        return usrIsCl;
+    }
+
+    public void setUsrIsCl(String usrIsCl) {
+        this.usrIsCl = usrIsCl;
+    }
+
+    public String getUsrMdfyDt() {
+        return usrMdfyDt;
+    }
+
+    public void setUsrMdfyDt(String usrMdfyDt) {
+        this.usrMdfyDt = usrMdfyDt;
+    }
+
+    public String getGdPrflImg() {
+        return gdPrflImg;
+    }
+
+    public void setGdPrflImg(String gdPrflImg) {
+        this.gdPrflImg = gdPrflImg;
+    }
+
+    public String getGdIdImg() {
+        return gdIdImg;
+    }
+
+    public void setGdIdImg(String gdIdImg) {
+        this.gdIdImg = gdIdImg;
+    }
+
+    public String getGdCbcImg() {
+        return gdCbcImg;
+    }
+
+    public void setGdCbcImg(String gdCbcImg) {
+        this.gdCbcImg = gdCbcImg;
+    }
+
+    public String getGdRpCntId() {
+        return gdRpCntId;
+    }
+
+    public void setGdRpCntId(String gdRpCntId) {
+        this.gdRpCntId = gdRpCntId;
+    }
+}
