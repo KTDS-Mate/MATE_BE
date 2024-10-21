9e31ad29feb2389d563e0f9826957241fc7c1048
MinGyu_Shin <sjfla3197@gmail.com>
2024-10-21 18:58:33 +0900

    fest: 의미없는 패키지 내 클래스 파일 삭제

--- src/main/java/com/mate/alram/dao/impl/AlarmDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.mate.alram.dao.impl;
-
-import com.mate.alram.dao.AlarmDao;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class AlarmDaoImpl extends SqlSessionDaoSupport implements AlarmDao {
-}
