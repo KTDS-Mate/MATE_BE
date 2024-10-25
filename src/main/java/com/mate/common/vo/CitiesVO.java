e78a207c604056318848ce30a1ae6de1c19a8182
kimjihun0514 <coma2390@naver.com>
2024-10-25 15:59:52 +0900

    feat:가이드 구인 게시글 등록 도시 추가

--- src/main/java/com/mate/common/vo/CitiesVO.java
+++ src/main/java/com/mate/common/vo/CitiesVO.java
@@ -2,6 +2,36 @@ package com.mate.common.vo;
 
 public class CitiesVO {
 
+	/**
+	 * 도시의 PK
+	 */
+	private int id;
+	/**
+	 * 도시 명 (영어)
+	 */
+	private String name;
+	/**
+	 * 국가 PK
+	 */
+	private int countryId;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
 	
 }
