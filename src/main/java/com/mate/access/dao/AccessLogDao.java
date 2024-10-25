10e3efa7ba28836236cc8f1c7304f1f290741eaa
MinGyu_Shin <sjfla3197@gmail.com>
2024-10-25 13:36:50 +0900

    Update AccessLogDao.java

--- src/main/java/com/mate/access/dao/AccessLogDao.java
+++ src/main/java/com/mate/access/dao/AccessLogDao.java
@@ -4,15 +4,9 @@ package com.mate.access.dao;
 public interface AccessLogDao {
 
 	public String NAMESPACE = "com.mate.access.dao.AccessLogDao";
-<<<<<<< HEAD
-	
-	public int insertNewAccessLog(AccessLogDao accessLogDao);
-	
-=======
 
 	public int insertNewAccessLog(AccessLogVO accessLogVO);
 
->>>>>>> main
 	public int selectLoginFailCount(String ip);
 
 }
