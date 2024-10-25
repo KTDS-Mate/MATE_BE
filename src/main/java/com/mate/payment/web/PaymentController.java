a2afc686be9f0157b4b4aeed95c048b794fd40a0
MinGyu_Shin <sjfla3197@gmail.com>
2024-10-25 13:51:55 +0900

    쓸데없는 파일 삭제

--- src/main/java/com/mate/payment/web/PaymentController.java
+++ src/main/java/com/mate/payment/web/PaymentController.java
@@ -24,13 +24,5 @@ public class PaymentController {
 		return "mypage/MyPageLayout_PaymentDetails";
 	}
 	
-	@GetMapping("/mypage/sales/gd-sales")
-	public String viewPaymentInfo(Model model, @PathVariable String id) {
-		// 회원VO는 세션으로 받아와야 하지만, 일단 PathVariable로 받아옴
-		// TODO 세션 추가 되면 그걸 받아와서 유저 정보 끼워서 만들기
-		PaymentListVO paymentListVO = this.paymentService.getAllMyPayment(id);
-		model.addAttribute("paymentListVO", paymentListVO);
-		return "mypage/MyPageLayout_PaymentDetails";
-	}
 	
 }
