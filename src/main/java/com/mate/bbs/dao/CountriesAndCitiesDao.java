a3043a32e89304f14b8fdd678d1165ef813b8c3c
YuriShin97 <phcomic@gmail.com>
2024-11-11 10:09:26 +0900

    1111 backend import * 수정

--- src/main/java/com/mate/bbs/dao/CountriesAndCitiesDao.java
+++ src/main/java/com/mate/bbs/dao/CountriesAndCitiesDao.java
@@ -2,7 +2,9 @@ package com.mate.bbs.dao;
 
 import java.util.List;
 
-import com.mate.common.vo.*;
+import com.mate.common.vo.RegionsVO;
+import com.mate.common.vo.CountriesVO;
+import com.mate.common.vo.CitiesVO;
 
 public interface CountriesAndCitiesDao {
 
--- src/main/java/com/mate/bbs/dao/impl/CountriesAndCitiesDaoImpl.java
+++ src/main/java/com/mate/bbs/dao/impl/CountriesAndCitiesDaoImpl.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.mate.common.vo.*;
+import com.mate.common.vo.RegionsVO;
+import com.mate.common.vo.CountriesVO;
+import com.mate.common.vo.CitiesVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
