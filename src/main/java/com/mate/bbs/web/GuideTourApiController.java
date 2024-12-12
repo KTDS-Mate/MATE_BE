7ff43cee60fef59bbcf51e6fd9c2a857515aa2ce
dhsong96 <thdekgka96@naver.com>
2024-12-12 13:04:52 +0900

    ddd

---
+++ src/main/java/com/mate/bbs/web/GuideTourApiController.java
@@ -0,0 +1,76 @@
+package com.mate.bbs.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.mate.bbs.service.GuideTourService;
+import com.mate.bbs.vo.GuideTourListVO;
+import com.mate.bbs.vo.GuideTourVO;
+import com.mate.bbs.vo.GuideTourWriteVO;
+import com.mate.bbs.vo.SearchGuideTourVO;
+import com.mate.common.vo.ApiResponse;
+import com.mate.user.vo.UserVO;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GuideTourApiController {
+
+	@Autowired
+	private GuideTourService guideTourService;
+	
+	/** 가이드가 등록한 투어 게시글 목록 조회 페이지 **/
+	@GetMapping("/guidetour/list")
+	public ApiResponse viewAllGuideTourPage(SearchGuideTourVO searchGuideTourVO ) {
+		
+		GuideTourListVO guideTourListVO = this.guideTourService.getAllGuideTour(searchGuideTourVO);
+		ApiResponse apiResponse = new ApiResponse();
+		apiResponse.setBody(guideTourListVO.getGuideTourList());
+		
+		return apiResponse;
+	}
+	/** 가이드가 등록한 게시글 상세 조회 페이지 **/
+	@GetMapping("/guidetour/info")
+	public ApiResponse viewOneGuideTourPage (@RequestBody String gdTrPstId) {
+		
+		GuideTourVO guideTourVO = this.guideTourService.getOneGuideTour(gdTrPstId);
+		ApiResponse apiResponse = new ApiResponse();
+		apiResponse.setBody(guideTourVO);
+		
+		return apiResponse;
+	}
+	
+	/**가이드가 작성한 투어 게시글을 받아와서 DB에 저장하는 페이지 **/
+	@PostMapping("/guidetour/insert")
+	public ApiResponse doCreateNewGuideTour(@RequestBody @Valid GuideTourWriteVO guideTourWriteVO,
+											BindingResult bindingResult,
+											@SessionAttribute(value = "_LOGIN_USER_", required = false) UserVO loginUserVO) {
+		
+		if(bindingResult.hasErrors()) {
+			List<String> fieldErrors = bindingResult.getFieldErrors()
+													.stream()
+													.map(fieldError -> fieldError.getDefaultMessage())
+													.toList();
+			ApiResponse errorResponse = new ApiResponse(HttpStatus.BAD_REQUEST);
+			errorResponse.setErrors(fieldErrors);
+			
+			return errorResponse;
+		}
+		
+		boolean isCreate = this.guideTourService.createNewGuideTour(guideTourWriteVO);
+		
+		return new ApiResponse(isCreate);
+	}
+	
+	
+}
