686172e19cc3910c0cf4761f3ca0b385db2c1a83
MinGyu_Shin <sjfla3197@gmail.com>
2024-12-13 16:17:34 +0900

    머지전 커밋

--- src/main/java/com/mate/mypage/web/api/MyPaymentApiController.java
+++ src/main/java/com/mate/mypage/web/api/MyPaymentApiController.java
@@ -1,8 +1,8 @@
 package com.mate.mypage.web.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +18,8 @@ public class MyPaymentApiController {
 	@Autowired
 	private PaymentService paymentService;
 	
-	@PostMapping("/payment/list")
-    public ApiResponse viewPaymentList(@RequestBody SearchPaymentVO searchPaymentVO) {
+	@GetMapping("/payment/list")
+    public ApiResponse viewPaymentList(@ModelAttribute SearchPaymentVO searchPaymentVO) {
 		// 회원VO는 세션으로 받아와야 하지만, 일단 PathVariable로 받아옴
 		// TODO 세션 추가 되면 그걸 받아와서 유저 정보 끼워서 만들기
 		PaymentListVO paymentListVO = this.paymentService.getAllMyPayment(searchPaymentVO);
