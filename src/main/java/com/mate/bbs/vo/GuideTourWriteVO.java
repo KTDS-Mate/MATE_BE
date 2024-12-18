a7c257b3687ebf511a9792218f2cda8a208a89af
kimjihun0514 <coma2390@naver.com>
2024-12-18 19:24:00 +0900

    머지 전 커밋

--- src/main/java/com/mate/bbs/vo/GuideTourWriteVO.java
+++ src/main/java/com/mate/bbs/vo/GuideTourWriteVO.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public class GuideTourWriteVO {
-	
+
 	/**
 	 * 가이드 투어 게시글 아이디
 	 */
@@ -49,8 +49,8 @@ public class GuideTourWriteVO {
 	/**
 	 * 투어 비용(가격)
 	 */
-	@Min(value=1 , message="1 이상의 값을 입력해주세요!")
-	@PositiveOrZero(message ="음수를 넣으실 수 없습니다!")
+	@Min(value = 1, message = "1 이상의 값을 입력해주세요!")
+	@PositiveOrZero(message = "음수를 넣으실 수 없습니다!")
 	private double gdTrPrc;
 	/**
 	 * 투어 최대 인원수
@@ -60,7 +60,7 @@ public class GuideTourWriteVO {
 	 * 투어를 진행할 도시 아이디 fk
 	 */
 	private int trCtId;
-	
+
 	/**
 	 * 투어 요약(요구사항)
 	 */
@@ -71,7 +71,7 @@ public class GuideTourWriteVO {
 	 */
 	private String gdTrRstrDt;
 	/**
-	 *  투어제공항목 리스트
+	 * 투어제공항목 리스트
 	 */
 	private List<GuideTourProvidedVO> guideTourProvidedList;
 	/**
@@ -90,119 +90,185 @@ public class GuideTourWriteVO {
 	 * 투어 작성자의 ID VO
 	 */
 	private String athrId;
-	
+
+	private String gdTrLat;
+	private String gdTrLng;
+	private String gdTrAddrs;
+
 	public String getGdTrPstId() {
 		return gdTrPstId;
 	}
+
 	public void setGdTrPstId(String gdTrPstId) {
 		this.gdTrPstId = gdTrPstId;
 	}
+
 	public String getGdTrTtl() {
 		return gdTrTtl;
 	}
+
 	public void setGdTrTtl(String gdTrTtl) {
 		this.gdTrTtl = gdTrTtl;
 	}
+
 	public String getInputYear() {
 		return inputYear;
 	}
+
 	public void setInputYear(String inputYear) {
 		this.inputYear = inputYear;
 	}
+
 	public String getInputStartHour() {
 		return inputStartHour;
 	}
+
 	public void setInputStartHour(String inputStartHour) {
 		this.inputStartHour = inputStartHour;
 	}
+
 	public String getGdTrStDt() {
 		return gdTrStDt;
 	}
+
 	public void setGdTrStDt(String gdTrStDt) {
 		this.gdTrStDt = gdTrStDt;
 	}
+
 	public String getGdTrEdDt() {
 		return gdTrEdDt;
 	}
+
 	public void setGdTrEdDt(String gdTrEdDt) {
 		this.gdTrEdDt = gdTrEdDt;
 	}
+
 	public String getInputEndHour() {
 		return inputEndHour;
 	}
+
 	public void setInputEndHour(String inputEndHour) {
 		this.inputEndHour = inputEndHour;
 	}
+
 	public String getGdTrPrps() {
 		return gdTrPrps;
 	}
+
 	public void setGdTrPrps(String gdTrPrps) {
 		this.gdTrPrps = gdTrPrps;
 	}
+
 	public String getGdTrMp() {
 		return gdTrMp;
 	}
+
 	public void setGdTrMp(String gdTrMp) {
 		this.gdTrMp = gdTrMp;
 	}
+
 	public double getGdTrPrc() {
 		return gdTrPrc;
 	}
+
 	public void setGdTrPrc(double gdTrPrc) {
 		this.gdTrPrc = gdTrPrc;
 	}
+
 	public int getGdTrMxNp() {
 		return gdTrMxNp;
 	}
+
 	public void setGdTrMxNp(int gdTrMxNp) {
 		this.gdTrMxNp = gdTrMxNp;
 	}
+
 	public int getTrCtId() {
 		return trCtId;
 	}
+
 	public void setTrCtId(int trCtId) {
 		this.trCtId = trCtId;
 	}
+
 	public String getGdTrSmry() {
 		return gdTrSmry;
 	}
+
 	public void setGdTrSmry(String gdTrSmry) {
 		this.gdTrSmry = gdTrSmry;
 	}
+
 	public String getGdTrRstrDt() {
 		return gdTrRstrDt;
 	}
+
 	public void setGdTrRstrDt(String gdTrRstrDt) {
 		this.gdTrRstrDt = gdTrRstrDt;
 	}
+
 	public List<GuideTourProvidedVO> getGuideTourProvidedList() {
 		return guideTourProvidedList;
 	}
+
 	public void setGuideTourProvidedList(List<GuideTourProvidedVO> guideTourProvidedList) {
 		this.guideTourProvidedList = guideTourProvidedList;
 	}
+
 	public List<GuideTourImgVO> getGuideTourImgList() {
 		return guideTourImgList;
 	}
+
 	public void setGuideTourImgList(List<GuideTourImgVO> guideTourImgList) {
 		this.guideTourImgList = guideTourImgList;
 	}
+
 	public List<GuideTourDetailInfoVO> getGuideTourDetailInfoList() {
 		return guideTourDetailInfoList;
 	}
+
 	public void setGuideTourDetailInfoList(List<GuideTourDetailInfoVO> guideTourDetailInfoList) {
 		this.guideTourDetailInfoList = guideTourDetailInfoList;
 	}
+
 	public List<GuideTourScheduleInfoVO> getGuideTourScheduleInfoList() {
 		return guideTourScheduleInfoList;
 	}
+
 	public void setGuideTourScheduleInfoList(List<GuideTourScheduleInfoVO> guideTourScheduleInfoList) {
 		this.guideTourScheduleInfoList = guideTourScheduleInfoList;
 	}
+
 	public String getAthrId() {
 		return athrId;
 	}
+
 	public void setAthrId(String athrId) {
 		this.athrId = athrId;
 	}
+
+	public String getGdTrLat() {
+		return gdTrLat;
+	}
+
+	public void setGdTrLat(String gdTrLat) {
+		this.gdTrLat = gdTrLat;
+	}
+
+	public String getGdTrLng() {
+		return gdTrLng;
+	}
+
+	public void setGdTrLng(String gdTrLng) {
+		this.gdTrLng = gdTrLng;
+	}
+
+	public String getGdTrAddrs() {
+		return gdTrAddrs;
+	}
+
+	public void setGdTrAddrs(String gdTrAddrs) {
+		this.gdTrAddrs = gdTrAddrs;
+	}
+
 }
